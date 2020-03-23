a02dac7114657a203183c3e4018f165e84f20d86
Andrew Naumov <anaumow@mail.ru>
2020-03-23 15:03:26 +0300

    refactoring

---
+++ spring-security-data-jpa-hibernate/src/main/java/ru/naumow/config/AspectJConfig.java
@@ -0,0 +1,11 @@
+package ru.naumow.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy()
+@ComponentScan("ru.naumow.aspects")
+public class AspectJConfig {
+}
